24cc35d05e872acf65a42a4004f68bde0cad66dd
Shanzay Khalid <shanzaykhalidfarooq@gmail.com>
2026-08-10 12:20:55 +0500

    Add program to calculate average of three numbers

---
+++ average.dart
@@ -0,0 +1,8 @@
+//take 3 numbers and print their average
+void main(){
+  double num1 = 30.0;
+  double num2 = 20.0;
+  double num3 = 10.0;
+  double avg = (num1 + num2 + num3)/3;
+  print("The average is : $avg");
+}
