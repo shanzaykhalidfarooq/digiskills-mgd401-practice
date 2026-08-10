905b92429e558e02dda046362a53cc24c0dec783
Shanzay Khalid <shanzaykhalidfarooq@gmail.com>
2026-08-10 14:52:45 +0500

    Add grade calculator based on student marks

---
+++ 02_grade_calculator.dart
@@ -0,0 +1,14 @@
+void main(){
+  double studentMarks = 74;
+  if(studentMarks >= 90){
+    print("You got A Grade");
+  }else if(studentMarks >= 80){
+    print("You got B Grade");
+  }else if(studentMarks >= 70){
+    print("You got C grade");
+  }else if(studentMarks >=60){
+    print("You got D grade");
+  }else{
+    print("You got F grade");
+  }
+}
